1d26638edd2a5b41240797af8f4da40aa62d3776
Lucia <luciasaradiez10@gmail.com>
2026-02-19 16:55:01 +1300

    added code to read password user and other details from .env

--- scr/db_connection.py
+++ scr/db_connection.py
@@ -1,9 +1,9 @@
 
 from sqlalchemy import create_engine
 
-def create_db_and_engine(user, password, host="localhost", port=5432, db_name="sales_db"):
+def create_db_and_engine(user, password, host, port, db_name):
     # Connect to base default 'postgres'
-    engine_default = create_engine("postgresql+psycopg2://postgres:dlasoidIND0034fj@localhost:5432/postgres")
+    engine_default = create_engine(f"postgresql+psycopg2://{user}:{password}@{host}:{port}/postgres")
 
     # Create new database
     engine_default = create_engine(f"postgresql+psycopg2://{user}:{password}@{host}:{port}/postgres")
--- scr/load_to_db.py
+++ scr/load_to_db.py
@@ -2,12 +2,22 @@
 
 from main_script import clean_sales_data
 from db_connection import create_db_and_engine
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+user = os.getenv("DB_USER")
+password = os.getenv("DB_PASSWORD")
+host = os.getenv("DB_HOST")
+port = os.getenv("DB_PORT")
+db_name = os.getenv("DB_NAME")
 
 # Clean data using main_script.py
 df_clean = clean_sales_data()
 
 # Connect to Postgres
-engine = create_db_and_engine(user="postgres", password="mypassword")
+engine = create_db_and_engine(user=user, password=password, host=host, port=port, db_name=db_name)
 
 # Upload to Postgres, replaces if already exists
 df_clean.to_sql('sales', engine, if_exists='replace', index=False)
