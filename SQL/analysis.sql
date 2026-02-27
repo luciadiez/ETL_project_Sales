e9bc831f7d716df0154900641611900b0de94231
Lucia <luciasaradiez10@gmail.com>
2026-02-27 15:30:21 +1300

    Added new column to dim_date in create_tables, created a query to understand some metrics

---
+++ SQL/analysis.sql
@@ -0,0 +1,81 @@
+
+-- Number of sales by day of the week
+SELECT 
+dat.day_of_week,
+count(DISTINCT transaction_id) as sales_count
+FROM transactions t
+LEFT JOIN dim_date dat ON t.transaction_date=dat.date
+GROUP BY dat.day_of_week
+ORDER BY sales_count DESC;
+
+-- Total spending by day of the week
+SELECT 
+dat.day_of_week,
+sum(quantity*price_per_unit) as total_spent
+FROM transactions t
+LEFT JOIN dim_date dat ON t.transaction_date=dat.date
+GROUP BY dat.day_of_week
+ORDER BY total_spent DESC;
+
+-- Number of sales and total spending by category
+SELECT
+p.category,
+count(DISTINCT transaction_id) as sales_count,
+sum(t.quantity*t.price_per_unit) as total_spent
+FROM transactions t
+LEFT JOIN products p ON p.item=t.item
+GROUP BY p.category
+ORDER BY total_spent DESC;
+
+-- 5 most expensive items
+SELECT 
+item_number,
+price_per_unit
+FROM products 
+ORDER BY price_per_unit DESC
+limit 5;
+
+-- Item that generates the highest total spending
+SELECT
+item,
+sum(quantity*price_per_unit) as total_spent
+FROM transactions
+GROUP by item
+ORDER by total_spent DESC 
+limit 1;
+
+-- Are most transactions online or in store? 
+SELECT
+    location,
+    count(DISTINCT transaction_id) as sales_count
+FROM transactions 
+GROUP by location
+ORDER by sales_count DESC;
+
+-- Are most online transactions made on the weekend?
+SELECT
+    weekend,
+    location,
+    count(DISTINCT transaction_id) as sales_count
+FROM transactions t
+LEFT JOIN dim_date dat ON dat.date=t.transaction_date
+where location= 'Online'
+GROUP by location,weekend
+ORDER BY sales_count DESC;
+
+-- What's the top payment method used?
+SELECT
+    payment_method,
+    count(DISTINCT transaction_id) as sales_count
+from transactions
+GROUP BY payment_method
+ORDER BY sales_count DESC
+LIMIT 1;
+
+-- Do clients spend more money using digital wallet and credit card than cash?
+SELECT
+payment_method,
+sum(quantity*price_per_unit) as total_spent
+FROM transactions
+GROUP BY payment_method
+ORDER by total_spent DESC;
--- SQL/create_tables.sql
+++ SQL/create_tables.sql
@@ -45,6 +45,7 @@ CREATE TABLE IF NOT EXISTS transactions (
     transaction_date DATE,
     discount_applied  BOOLEAN,
     quantity INT,
+    price_per_unit FLOAT,
     PRIMARY KEY (transaction_id),
     FOREIGN KEY (item) REFERENCES products(item),
     FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
@@ -58,7 +59,8 @@ INSERT INTO transactions (
             location,
             transaction_date,
             discount_applied,
-            quantity
+            quantity,
+            price_per_unit
 )
     SELECT distinct transaction_id,
             item,
@@ -67,9 +69,19 @@ INSERT INTO transactions (
             location,
             transaction_date,
             discount_applied,
-            quantity
+            quantity,
+            price_per_unit
     FROM sales;
 
-
-
-
+-- Create dim_date
+CREATE TABLE IF NOT EXISTS dim_date AS
+SELECT
+    cast(date as date) AS date,
+    EXTRACT(YEAR FROM date) AS year,
+    EXTRACT(MONTH FROM date) AS month,
+    EXTRACT(DAY FROM date) AS day,
+    TO_CHAR(date, 'Day') AS day_of_week,
+    CAST(EXTRACT(DOW FROM date) as INT)+1 AS dow,
+    case when CAST(EXTRACT(DOW FROM date) as INT)+1 in (1,7) then 'TRUE'
+        else 'FALSE' end as weekend
+FROM generate_series('2000-01-01'::date, '2030-12-31', '1 day') AS date;
